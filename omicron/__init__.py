0e55695e9cd631c0c98d12d2ec9ceccbd6a245fa
jieyu <code@jieyu.ai>
2020-11-08 23:57:15 +0800

    Bump version: 0.1.2 → 0.2.0

--- omicron/__init__.py
+++ omicron/__init__.py
@@ -1,10 +1,16 @@
 """Top-level package for omicron."""
 
 __author__ = """Aaron Yang"""
-__email__ = 'code@jieyu.ai'
-__version__ = '0.1.2'
+__email__ = "code@jieyu.ai"
+__version__ = "__version__ = '0.2.0'"
 
 import logging
+
+import cfg4py
+
+from .dal.cache import cache
+from .dal.postgres import db
+from .dal.postgres import init as init_db
 
 logger = logging.getLogger(__name__)
 
@@ -12,10 +18,12 @@ logger = logging.getLogger(__name__)
 # 远程接口来获取数据
 _local_fetcher = None
 
+# 是否配置了数据库（Redis必须配置）
+_has_db = False
+
 
 async def init(fetcher=None):
     """
-
     Args:
         fetcher: instance of AbstractQuotesFetcher。如果不为None,则Omicron会使用这个fetcher
         来获取行情数据，否则使用远程接口。
@@ -23,17 +31,38 @@ async def init(fetcher=None):
     Returns:
 
     """
-    global _local_fetcher
+    # to avoid circular import
+    from .models.securities import Securities
 
-    from omicron.dal import cache
-    from omicron.models.securities import Securities
-
+    global _local_fetcher, cache, _has_db
     _local_fetcher = fetcher
+
     await cache.init()
     sec = Securities()
     await sec.load()
+
+    cfg = cfg4py.get_instance()
+    try:
+        dsn = cfg.postgres.dsn
+    except AttributeError:
+        pass
+    else:  # user configured valid dsn, then init database connection
+        _has_db = True
+        await init_db(dsn)
+
+
+async def shutdown():
+    await db.pop_bind().close()
+
 
 def get_local_fetcher():
     global _local_fetcher
 
     return _local_fetcher
+
+
+def has_db():
+    return _has_db
+
+
+__all__ = ["cache", "db"]
