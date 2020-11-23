71a8d4b54e3695079060a87a70fc8a72026ac520
jieyu <code@jieyu.ai>
2020-11-23 10:23:11 +0800

    Bump version: 0.3.0 → 0.4.0

--- omicron/__init__.py
+++ omicron/__init__.py
@@ -2,6 +2,7 @@
 
 __author__ = """Aaron Yang"""
 __email__ = "code@jieyu.ai"
+__version__ = "__version__ = '0.4.0'"
 
 import logging
 
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    "pyemit >= 0.4.0",
+    "cfg4py == 0.8.0",
+    "arrow == 0.15",
+    "numpy == 1.19.4",
+    "aioredis == 1.3.1",
+    "numba == 0.49.1",
+    "SQLAlchemy == 1.3.20",
+    "gino == 1.0.1",
+    "asyncpg == 0.21.0",
+    "aiohttp >= 3.7",
+    "sh == 1.14.1",
+]
+
+setup_requirements = []
+
+test_requirements = [
+    "zillionare-omega == 0.6.0",
+    "zillionare-omega-adaptors-jq == 0.2.3",
+]
+
+setup(
+    author="Aaron Yang",
+    author_email="code@jieyu.ai",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Core module for Zillionare",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="omicron",
+    name="zillionare-omicron",
+    packages=find_packages(include=["omicron", "omicron.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/zillionare/omicron",
+    version="version='0.4.0'",
+    zip_safe=False,
+)
