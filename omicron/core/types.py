c72ccf8dd2222432fb788067508edbf404d8eb3a
jieyu <code@jieyu.ai>
2021-01-31 22:49:29 +0800

    ✨feat: add serialization support to types

--- omicron/core/types.py
+++ omicron/core/types.py
@@ -19,6 +19,61 @@ class FrameType(Enum):
     QUARTER = "1Q"
     YEAR = "1Y"
 
+    def to_int(self) -> int:
+        mapping = {
+            FrameType.MIN1: 1,
+            FrameType.MIN5: 2,
+            FrameType.MIN15: 3,
+            FrameType.MIN30: 4,
+            FrameType.MIN60: 5,
+            FrameType.DAY: 6,
+            FrameType.WEEK: 7,
+            FrameType.MONTH: 8,
+            FrameType.QUARTER: 9,
+            FrameType.YEAR: 10,
+        }
+        return mapping[self]
+
+    @staticmethod
+    def from_int(frame_type: int) -> "FrameType":
+        mapping = {
+            1: FrameType.MIN1,
+            2: FrameType.MIN5,
+            3: FrameType.MIN15,
+            4: FrameType.MIN30,
+            5: FrameType.MIN60,
+            6: FrameType.DAY,
+            7: FrameType.WEEK,
+            8: FrameType.MONTH,
+            9: FrameType.QUARTER,
+            10: FrameType.YEAR,
+        }
+
+        return mapping[frame_type]
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.to_int() < other.to_int()
+        return NotImplemented
+
+    def __le__(self, other) -> bool:
+        if self.__class__ is other.__class__:
+            return self.to_int() <= other.to_int()
+
+        return NotImplemented
+
+    def __ge__(self, other) -> bool:
+        if self.__class__ is other.__class__:
+            return self.to_int() >= other.to_int()
+
+        return NotImplemented
+
+    def __gt__(self, other) -> bool:
+        if self.__class__ is other.__class__:
+            return self.to_int() > other.to_int()
+
+        return NotImplemented
+
 
 class SecurityType(Enum):
     STOCK = "stock"
---
+++ tests/test_types.py
@@ -0,0 +1,40 @@
+import logging
+import unittest
+from unittest.mock import patch
+
+from omicron.core.types import FrameType
+
+logger = logging.getLogger(__name__)
+
+
+class MyTestCase(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_frame_type_convert(self):
+        for frame_type in FrameType:
+            self.assertEqual(
+                frame_type.to_int(), FrameType.from_int(frame_type.to_int()).to_int()
+            )
+
+    def test_comparison(self):
+        day = FrameType("1d")
+        week = FrameType("1w")
+        month = FrameType("1M")
+        quater = FrameType("1Q")
+        year = FrameType("1Y")
+        min_1 = FrameType("1m")
+        min_5 = FrameType("5m")
+
+        self.assertTrue(week > day)
+        self.assertTrue(day < week)
+        self.assertTrue(week < month)
+        self.assertTrue(month < quater)
+        self.assertTrue(quater < year)
+        self.assertTrue(min_1, min_5)
+
+        self.assertTrue(day >= day)
+        self.assertTrue(day <= day)
