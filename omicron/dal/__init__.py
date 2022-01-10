075e81d2c0e8fe32551ba612ed6c5140b3ade145
evimacs <evimacs@hotmail.com>
2022-01-10 09:15:27 +0000

    ✨ feat: 股票支持存入influxdb

--- omicron/dal/__init__.py
+++ omicron/dal/__init__.py
@@ -1,4 +1,4 @@
 from omicron.dal.cache import cache
 from omicron.dal.postgres import db, init
-
-__all__ = ["init", "db", "cache"]
+from omicron.dal.influxdb import influxdb
+__all__ = ["init", "db", "cache", "influxdb"]
---
+++ tests/models/test_funds.py
@@ -0,0 +1,29 @@
+import logging
+import unittest
+from unittest.mock import patch
+import arrow
+import omicron
+from tests import init_test_env
+from omicron.models.funds import Funds
+
+logger = logging.getLogger(__name__)
+
+
+class FundsTest(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        self.cfg = init_test_env()
+
+        self.cfg.postgres.enabled = True
+        await omicron.init()
+
+    async def asyncTearDown(self) -> None:
+        await omicron.shutdown()
+
+    async def test_crud(self):
+        await Funds.truncate()
+
+        date = arrow.get("2021-12-21")
+
+        logger.info("setp 1: fetch from remote")
+        recs = await Funds.get("512690", date)
+        self.assertEqual(recs[0]["code"], "512690")
