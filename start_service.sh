ed4af40a95f68a433825171560235da51119df25
aaron yang <aaron_yang@jieyu.ai>
2022-09-03 15:37:02 +0800

    🐳 chore: remove minio from test env

--- start_service.sh
+++ start_service.sh
@@ -3,7 +3,7 @@ if [ $IS_GITHUB ]; then
     exit 0
 fi
 
-echo "本地测试环境，将初始化redis, minio和influxdb!"
+echo "本地测试环境，将初始化redis和influxdb!"
 
 export TZ=Asia/Shanghai
 sudo -E apt-get update
@@ -16,5 +16,5 @@ sudo docker run -d -p 8086:8086 --name tox-influxdb influxdb
 sleep 5
 sudo docker exec -i tox-influxdb bash -c 'influx setup --username my-user --password my-password --org my-org --bucket my-bucket --token my-token --force'
 
-echo "初始化minio容器"
-sudo docker run -d -p 9000:9000 -p 9001:9001 --name tox-minio minio/minio server /tmp --console-address ":9001"
+# echo "初始化minio容器"
+# sudo docker run -d -p 9000:9000 -p 9001:9001 --name tox-minio minio/minio server /tmp --console-address ":9001"
--- stop_service.sh
+++ stop_service.sh
@@ -3,10 +3,10 @@ if [ $IS_GITHUB ]; then
     exit 0
 fi
 
-echo "将移除start_service脚本启动的本地环境中的redis, minio和influxdb容器!"
+echo "将移除start_service脚本启动的本地环境中的redis, influxdb容器!"
 
 sudo docker rm -f tox-redis
-sudo docker rm -f tox-minio
+# sudo docker rm -f tox-minio
 sudo docker rm -f tox-influxdb
 
 sudo docker ps -a
